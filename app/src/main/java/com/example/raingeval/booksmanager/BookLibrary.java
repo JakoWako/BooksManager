fa4d925fbdbca243dd789d560fb8dadb004be5b3
antcho <antcho60@gmail.com>
2015-11-01 15:58:20 +0100

    Création branche pour développement de la BDD.

--- app/src/main/java/com/example/raingeval/booksmanager/BookLibrary.java
+++ app/src/main/java/com/example/raingeval/booksmanager/BookLibrary.java
@@ -4,7 +4,7 @@ import android.graphics.drawable.Drawable;
 
 import java.util.ArrayList;
 import java.util.List;
-
+//autre tst de branche
 /**
  * Created by raingeval on 24/09/15.
  */
--- app/src/main/java/com/example/raingeval/booksmanager/C1.java
+++ app/src/main/java/com/example/raingeval/booksmanager/C1.java
@@ -3,5 +3,7 @@ package com.example.raingeval.booksmanager;
 /**
  * Created by wachowiak on 08/10/15.
  */
+
 public class C1 {
+    //test voir si ça s'écrit bien dans la branche BDD.
 }
